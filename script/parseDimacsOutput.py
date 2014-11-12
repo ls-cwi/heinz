109d56437bfbc89bd200cbfee048e146d9e3d627
Mohammed El-Kebir <m.el-kebir@cwi.nl>
2014-11-12 19:22:36 +0100

    Script to parse DIMACS output

---
+++ script/parseDimacsOutput.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+
+with open(sys.argv[1]) as f:
+  sys.stdout.write('"'+sys.argv[1]+'"')
+  for line in f:
+    s = line.rstrip("\n").split()
+    if len(s) == 0:
+      continue
+    if s[0] == "Name":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Problem":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Program":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Threads":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Time":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Dual":
+      sys.stdout.write(","+s[1])
+    elif s[0] == "Primal":
+      sys.stdout.write(","+s[1])
+sys.stdout.write("\n")
