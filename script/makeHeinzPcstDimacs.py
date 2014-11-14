5a09d4cddb7e621b2b9ff52027fe9580d8abb9bc
Mohammed El-Kebir <m.el-kebir@cwi.nl>
2014-11-14 22:04:20 +0100

    Integrated MC in PBS generation scripts

--- script/makeHeinzPcstDimacs.py
+++ script/makeHeinzPcstDimacs.py
@@ -11,6 +11,7 @@ full_output_dir = sys.argv[3]
 filename1 = full_output_dir + "/" + "1-" + filename
 filename2 = full_output_dir + "/" + "2-" + filename
 filename3 = full_output_dir + "/" + "3-" + filename
+filename4 = full_output_dir + "/" + "4-" + filename
 timelimit_pbs = sys.argv[4]
 timelimit_heinz = sys.argv[5]
 
@@ -23,4 +24,5 @@ print "cd ~/DIMACS2014/"
 print "( /usr/bin/time -o " + filename1 + ".time bin/heinz_pcst_no_pre " + full_filename + " " + timelimit_heinz + " 2 " + filename1 + ".dimacs" + " > " + filename1 + ".out 2> " + filename1 + ".err ) &"
 print "( /usr/bin/time -o " + filename2 + ".time bin/heinz_pcst_no_dc " + full_filename + " " + timelimit_heinz + " 2 " + filename2 + ".dimacs" + " > " + filename2 + ".out 2> " + filename2 + ".err ) &"
 print "( /usr/bin/time -o " + filename3 + ".time bin/heinz_pcst_dc " + full_filename + " " + timelimit_heinz + " 2 " + filename3 + ".dimacs" + " > " + filename3 + ".out 2> " + filename3 + ".err ) &"
+print "( /usr/bin/time -o " + filename4 + ".time bin/heinz_pcst_mc " + full_filename + " " + timelimit_heinz + " 2 " + filename4 + ".dimacs" + " > " + filename4 + ".out 2> " + filename4 + ".err ) &"
 print "wait"
