68335747e90299c1a553910de76afe2e4560ebda
Gunnar W. Klau <gunnar.klau@cwi.nl>
2015-01-29 13:53:06 +0100

    nothing changed

--- src/preprocessing/negmirroredhubs.h
+++ src/preprocessing/negmirroredhubs.h
@@ -11,128 +11,128 @@
 #include "rule.h"
 
 namespace nina {
-namespace mwcs {
-
-template<typename GR,
-         typename WGHT = typename GR::template NodeMap<double> >
-class NegMirroredHubs : public Rule<GR, WGHT>
-{
-public:
-  typedef GR Graph;
-  typedef WGHT WeightNodeMap;
-  typedef Rule<GR, WGHT> Parent;
-  typedef typename Parent::NodeMap NodeMap;
-  typedef typename Parent::NodeSet NodeSet;
-  typedef typename Parent::NodeSetIt NodeSetIt;
-  typedef typename Parent::NodeSetMap NodeSetMap;
-  typedef typename Parent::DegreeNodeMap DegreeNodeMap;
-  typedef typename Parent::DegreeNodeSetVector DegreeNodeSetVector;
-  typedef typename Parent::LabelNodeMap LabelNodeMap;
-
-  TEMPLATE_GRAPH_TYPEDEFS(Graph);
-
-  using Parent::remove;
-  using Parent::merge;
-
-  NegMirroredHubs();
-  virtual ~NegMirroredHubs() {}
-  virtual int apply(Graph& g,
-                    const NodeSet& rootNodes,
-                    LabelNodeMap& label,
-                    WeightNodeMap& score,
-                    NodeSetMap& mapToPre,
-                    NodeSetMap& preOrigNodes,
-                    NodeSetMap& neighbors,
-                    int& nNodes,
-                    int& nArcs,
-                    int& nEdges,
-                    DegreeNodeMap& degree,
-                    DegreeNodeSetVector& degreeVector,
-                    double& LB);
-
-  virtual std::string name() const { return "NegMirroredHubs"; }
-};
-
-template<typename GR, typename WGHT>
-inline NegMirroredHubs<GR, WGHT>::NegMirroredHubs()
-  : Parent()
-{
-}
-
-template<typename GR, typename WGHT>
-inline int NegMirroredHubs<GR, WGHT>::apply(Graph& g,
-                                            const NodeSet& rootNodes,
-                                            LabelNodeMap& label,
-                                            WeightNodeMap& score,
-                                            NodeSetMap& mapToPre,
-                                            NodeSetMap& preOrigNodes,
-                                            NodeSetMap& neighbors,
-                                            int& nNodes,
-                                            int& nArcs,
-                                            int& nEdges,
-                                            DegreeNodeMap& degree,
-                                            DegreeNodeSetVector& degreeVector,
-                                            double& LB)
-{
-  typedef std::pair<double, Node> WeightNodePair;
-  typedef std::set<WeightNodePair> WeightNodePairSet;
-  typedef typename WeightNodePairSet::const_iterator WeightNodePairSetIt;
-  
-  typedef std::pair<Node, Node> NodePair;
-  typedef std::map<NodePair, WeightNodePairSet> NodePairMap;
-  typedef typename NodePairMap::const_iterator NodePairMapIt;
-
-  NodeSet negHubsToRemove;
-  for (size_t d = 3; d < degreeVector.size(); ++d)
-  {
-    const NodeSet& nodes = degreeVector[d];
-    if (nodes.size() > 1000) continue;
-    for (NodeSetIt nodeIt1 = nodes.begin(); nodeIt1 != nodes.end(); ++nodeIt1)
+  namespace mwcs {
+    
+    template<typename GR,
+    typename WGHT = typename GR::template NodeMap<double> >
+    class NegMirroredHubs : public Rule<GR, WGHT>
     {
-      Node u = *nodeIt1;
-      if (score[u] > 0) continue;
+    public:
+      typedef GR Graph;
+      typedef WGHT WeightNodeMap;
+      typedef Rule<GR, WGHT> Parent;
+      typedef typename Parent::NodeMap NodeMap;
+      typedef typename Parent::NodeSet NodeSet;
+      typedef typename Parent::NodeSetIt NodeSetIt;
+      typedef typename Parent::NodeSetMap NodeSetMap;
+      typedef typename Parent::DegreeNodeMap DegreeNodeMap;
+      typedef typename Parent::DegreeNodeSetVector DegreeNodeSetVector;
+      typedef typename Parent::LabelNodeMap LabelNodeMap;
       
-      const NodeSet& neighbors_u = neighbors[u];
-
-      for (NodeSetIt nodeIt2 = nodeIt1; nodeIt2 != nodes.end(); ++nodeIt2)
+      TEMPLATE_GRAPH_TYPEDEFS(Graph);
+      
+      using Parent::remove;
+      using Parent::merge;
+      
+      NegMirroredHubs();
+      virtual ~NegMirroredHubs() {}
+      virtual int apply(Graph& g,
+                        const NodeSet& rootNodes,
+                        LabelNodeMap& label,
+                        WeightNodeMap& score,
+                        NodeSetMap& mapToPre,
+                        NodeSetMap& preOrigNodes,
+                        NodeSetMap& neighbors,
+                        int& nNodes,
+                        int& nArcs,
+                        int& nEdges,
+                        DegreeNodeMap& degree,
+                        DegreeNodeSetVector& degreeVector,
+                        double& LB);
+      
+      virtual std::string name() const { return "NegMirroredHubs"; }
+    };
+    
+    template<typename GR, typename WGHT>
+    inline NegMirroredHubs<GR, WGHT>::NegMirroredHubs()
+    : Parent()
+    {
+    }
+    
+    template<typename GR, typename WGHT>
+    inline int NegMirroredHubs<GR, WGHT>::apply(Graph& g,
+                                                const NodeSet& rootNodes,
+                                                LabelNodeMap& label,
+                                                WeightNodeMap& score,
+                                                NodeSetMap& mapToPre,
+                                                NodeSetMap& preOrigNodes,
+                                                NodeSetMap& neighbors,
+                                                int& nNodes,
+                                                int& nArcs,
+                                                int& nEdges,
+                                                DegreeNodeMap& degree,
+                                                DegreeNodeSetVector& degreeVector,
+                                                double& LB)
+    {
+      typedef std::pair<double, Node> WeightNodePair;
+      typedef std::set<WeightNodePair> WeightNodePairSet;
+      typedef typename WeightNodePairSet::const_iterator WeightNodePairSetIt;
+      
+      typedef std::pair<Node, Node> NodePair;
+      typedef std::map<NodePair, WeightNodePairSet> NodePairMap;
+      typedef typename NodePairMap::const_iterator NodePairMapIt;
+      
+      NodeSet negHubsToRemove;
+      for (size_t d = 3; d < degreeVector.size(); ++d)
       {
-        Node v = *nodeIt2;
-        if (u == v) continue;
-        if (score[v] > 0) continue;
-        
-        if (neighbors_u == neighbors[v])
+        const NodeSet& nodes = degreeVector[d];
+        if (nodes.size() > 1000) continue;
+        for (NodeSetIt nodeIt1 = nodes.begin(); nodeIt1 != nodes.end(); ++nodeIt1)
         {
-          // either u or v needs to go
-          if (score[u] < score[v])
-            negHubsToRemove.insert(u);
-          else
-            negHubsToRemove.insert(v);
+          Node u = *nodeIt1;
+          if (score[u] > 0) continue;
+          
+          const NodeSet& neighbors_u = neighbors[u];
+          
+          for (NodeSetIt nodeIt2 = nodeIt1; nodeIt2 != nodes.end(); ++nodeIt2)
+          {
+            Node v = *nodeIt2;
+            if (u == v) continue;
+            if (score[v] > 0) continue; // we could also comment this out
+            
+            if (neighbors_u == neighbors[v])
+            {
+              // either u or v needs to go
+              if (score[u] < score[v])
+                negHubsToRemove.insert(u);
+              else
+                negHubsToRemove.insert(v);
+            }
+          }
         }
       }
+      
+      for (NodeSetIt nodeIt = rootNodes.begin(); nodeIt != rootNodes.end(); ++nodeIt)
+      {
+        // only remove if v is not a root node
+        negHubsToRemove.erase(*nodeIt);
+      }
+      
+      for (NodeSetIt nodeIt = negHubsToRemove.begin();
+           nodeIt != negHubsToRemove.end(); ++nodeIt)
+      {
+        Node v = *nodeIt;
+        
+        assert(rootNodes.find(v) == rootNodes.end());
+        remove(g, mapToPre, preOrigNodes, neighbors,
+               nNodes, nArcs, nEdges,
+               degree, degreeVector, v);
+      }
+      
+      return static_cast<int>(negHubsToRemove.size());
     }
-  }
-  
-  for (NodeSetIt nodeIt = rootNodes.begin(); nodeIt != rootNodes.end(); ++nodeIt)
-  {
-    // only remove if v is not a root node
-    negHubsToRemove.erase(*nodeIt);
-  }
-  
-  for (NodeSetIt nodeIt = negHubsToRemove.begin();
-       nodeIt != negHubsToRemove.end(); ++nodeIt)
-  {
-    Node v = *nodeIt;
     
-    assert(rootNodes.find(v) == rootNodes.end());
-    remove(g, mapToPre, preOrigNodes, neighbors,
-           nNodes, nArcs, nEdges,
-           degree, degreeVector, v);
-  }
-  
-  return static_cast<int>(negHubsToRemove.size());
-}
-
-} // namespace mwcs
+  } // namespace mwcs
 } // namespace nina
 
 #endif // NEGMIRROREDHUBS_H
