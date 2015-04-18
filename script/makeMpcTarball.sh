747cf663664385356f3dca9e968d1bf1bd901e8c
Mohammed El-Kebir <m.elkebir@gmail.com>
2015-04-18 08:39:16 -0400

    Updated script

---
+++ script/makeMpcTarball.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -cvjf heinz_src.tar.bz2 heinz/src heinz/README.md heinz/CMakeLists.txt heinz/LICENSE
---
+++ script/makeMpcTarballData.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -cvjf heinz_data.tar.bz2 heinz/data/test heinz/data/DIMACS/mwcs heinz/data/DIMACS/PCSPG-JMP heinz/data/DIMACS/PCSPG-CRR heinz/data/DIMACS/PCSPG-PUCNU heinz/data/DIMACS/i640 heinz/data/DIMACS/H heinz/data/DIMACS/H2 heinz/data/DIMACS/Random_graphs
